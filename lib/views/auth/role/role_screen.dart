4257431ab7a617c94ff6daa9c10171344bf003e7
MuhammadMutayyabHussain <hussainmutayyab214@gmail.com>
2024-11-15 22:48:26 +0500

    Onboarding Screens Updated Design

--- lib/views/auth/role/role_screen.dart
+++ lib/views/auth/role/role_screen.dart
@@ -8,11 +8,62 @@ import 'package:tasteclip/utils/app_string.dart';
 import 'package:tasteclip/views/auth/role/role_controller.dart';
 import 'package:tasteclip/widgets/app_background.dart';
 import 'package:tasteclip/widgets/app_button.dart';
-import '../../../widgets/or_continue_with.dart';
 
 class RoleScreen extends StatelessWidget {
   RoleScreen({super.key});
   final controller = Get.put(RoleController());
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Container(
+          height: 300,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [AppColors.lightColor, AppColors.whiteColor],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              AppButton(
+                text: AppString.signup,
+                onPressed: () {},
+                btnRadius: 30,
+              ),
+              14.vertical,
+              AppButton(
+                text: AppString.signin,
+                onPressed: () {},
+                btnRadius: 30,
+              ),
+              14.vertical,
+              TextButton.icon(
+                onPressed: () {},
+                label: Text(
+                  AppString.continueWithGoogle,
+                  style: AppTextStyles.lightStyle.copyWith(
+                    color: AppColors.mainColor,
+                  ),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,43 +74,41 @@ class RoleScreen extends StatelessWidget {
           body: Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      AppAssets.appLogo,
-                      width: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    AppAssets.appLogo,
+                    width: 200,
+                  ),
+                  56.vertical,
+                  Text(
+                    AppString.getStartedAs,
+                    style: AppTextStyles.boldBodyStyle
+                        .copyWith(color: AppColors.textColor),
+                  ),
+                  30.vertical,
+                  AppButton(
+                    text: AppString.user,
+                    onPressed: () => _showBottomSheet(context),
+                    btnRadius: 30,
+                  ),
+                  16.vertical,
+                  AppButton(
+                    text: AppString.restaurantManager,
+                    onPressed: () {},
+                    btnRadius: 30,
+                  ),
+                  10.vertical,
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      AppString.continueAsGuest,
+                      style: AppTextStyles.lightStyle,
                     ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(AppString.getStartedWith,
-                          style: AppTextStyles.boldBodyStyle.copyWith(
-                            color: AppColors.textColor,
-                          )),
-                    ),
-                    14.vertical,
-                    AppButton(
-                      text: AppString.user,
-                      onPressed: controller.goToUserAuthSecreen,
-                    ),
-                    15.vertical,
-                    AppButton(
-                      text: AppString.guest,
-                      onPressed: () {},
-                    ),
-                    30.vertical,
-                    const OrContinueWith(),
-                    20.vertical,
-                    AppButton(
-                      isGradient: false,
-                      btnColor: AppColors.primaryColor,
-                      text: AppString.restaurantManager,
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
+                  ),
+                  54.vertical,
+                ],
               ),
             ),
           ),
